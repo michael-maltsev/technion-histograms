e2f4daa1a69db10cfeb798b85263c3583e055f30
michael-maltsev <mmm.maltsev+personal@gmail.com>
2020-08-03 18:59:13 +0300

    Remove separate json files for each histogram

--- deploy.php
+++ deploy.php
@@ -116,6 +116,9 @@ function process_course($course, &$stats) {
 
             $data = json_decode(file_get_contents($filename), true);
 
+            # Less files - less timeout errors in Github Pages deployment.
+            unlink($filename);
+
             if ($data['max'] >= 200) {
                 log_warning("$course/$semester/$category: Data with invalid max: {$data['max']}");
             }
