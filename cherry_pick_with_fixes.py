347db9c6bfd0b0ecefd7ae98d4c165243bbbe6cd
michael-maltsev <36999014+michael-maltsev@users.noreply.github.com>
2025-03-15 21:10:13 +0200

    # Remove incorrect entry from COURSE_ALTERNATIVE_NAMES

--- cherry_pick_with_fixes.py
+++ cherry_pick_with_fixes.py
@@ -167,7 +167,6 @@ COURSE_ALTERNATIVE_NAMES = {
     'כימיה אורגנית מורחב 1': 'Organic Chemistry 1/ Extended',
     'יסודות היזמות': 'Innovation Masterclass',
     'מושגי יסוד במתמטיקה': 'Basic Concepts in Mathematics',
-    'משוואות דיפ\' רגילות ואינפי 2ח\'': 'עקרונות פיזיקליים של התקני מל"מ',
     'מערכות ליניאריות מ\' – בינלאומי': 'Linear Systems M – International',
     'חשבון דיפרנציאלי ואינטגרלי 1מ\' - בינלאומי': 'Differential and Integral Calculus 1M - International',
     'פיסיקה 1 - בינלאומי': 'Physics 1 - International',
